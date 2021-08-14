754564328c7bb9e8a78f6f5eb9bfc3fddc9c00c1
Divyam Gupta <dgupta92@gatech.edu>
2021-08-14 11:30:38 +0530

    Add rook moves

--- src/Piece/Rook.java
+++ src/Piece/Rook.java
@@ -1,5 +1,6 @@
 package Piece;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -11,9 +12,17 @@ public class Rook extends Piece {
     }
 
     @Override
-    List<int[]> findMoves(int i, int j) {
-        return null;
+    public List<int[]> findMoves(int i, int j) {
+        List<int[]> result = new ArrayList<>();
+        for (int a = 0; a < 8; a++) {
+            if (a != i) result.add(new int[]{a, j});
+        }
+        for (int b = 0; b < 8; b++) {
+            if (b != j) result.add(new int[]{i, b});
+        }
+        return result;
     }
+
     @Override
     public String toString() {
         return "R";
